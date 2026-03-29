1c06961dd7ed39dd3060e5d47b88adcd4bc8aae4
H4S1NTH4 <jmhdilsha@gmail.com>
2026-03-29 19:44:59 +0530

    fix(firmware): update sensor IDs and adjust telemetry topic setup

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -15,8 +15,11 @@ const char* MQTT_PASSWORD = "Agriwizard@999";
 const char* DEVICE_ID = "esp32-greenhouse-01";
 
 const char* SENSOR_ID = "REPLACE_WITH_SENSOR_ID_FROM_HARDWARE_SERVICE";
-const char* FAN_EQUIPMENT_ID = "b8cff3e4-7bc3-4480-a23c-a8bfae62aac1";
-const char* PUMP_EQUIPMENT_ID = "REPLACE_WITH_PUMP_EQUIPMENT_ID";
+const char* SOIL_SENSOR_ID = "REPLACE_WITH_SENSOR_ID_FROM_HARDWARE_SERVICE";
+const char* TMP_SENSOR_ID = "REPLACE_WITH_SENSOR_ID_FROM_HARDWARE_SERVICE";
+
+const char* FAN_EQUIPMENT_ID = "b8cff3e4-7bc3-4480-a23c-a8bfae62aac1"; //main wp
+const char* PUMP_EQUIPMENT_ID = "57aec2ac-8ce2-45fb-a037-ffacb8e9dd06"; //main wsply
 
 const char* PARAM_TEMP = "air_temp_c";
 const char* PARAM_HUMIDITY = "air_humidity_pct";
@@ -24,7 +27,7 @@ const char* PARAM_SOIL = "soil_moisture_pct";
 
 const int DHT_PIN = 4;
 const int DHT_TYPE = DHT11;
-const int SOIL_PIN = 34;
+const int SOIL_PIN = 26;
 const int FAN_PIN = 18;
 const int PUMP_PIN = 19;
 
@@ -164,8 +167,8 @@ float readSoilMoisturePct() {
 }
 
 void publishTelemetry() {
-  float humidity = 40.55 //dht.readHumidity();
-  float temperature = 25.6 //dht.readTemperature();
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
   float soilPct = readSoilMoisturePct();
 
   if (isnan(humidity) || isnan(temperature)) {
@@ -247,6 +250,9 @@ void connectMQTT() {
 
 void setupTopics() {
   sensorTelemetryTopic = "agriwizard/sensor/" + String(SENSOR_ID) + "/telemetry";
+  soilSensorTelemetryTopic = "agriwizard/sensor/" + String(SOIL_SENSOR_ID) + "/telemetry";
+  tmpSensorTelemetryTopic = "agriwizard/sensor/" + String(TMP_SENSOR_ID) + "/telemetry";
+
   fanCommandTopic = "agriwizard/equipment/" + String(FAN_EQUIPMENT_ID) + "/command";
   pumpCommandTopic = "agriwizard/equipment/" + String(PUMP_EQUIPMENT_ID) + "/command";
   fanStatusTopic = fanCommandTopic + "/status";
@@ -268,6 +274,8 @@ void setup() {
 
   setupTopics();
   logLine("sensor topic=" + sensorTelemetryTopic);
+  logLine("soil sensor topic=" + soilSensorTelemetryTopic);
+  logLine("temp sensor topic=" + tmpSensorTelemetryTopic);
   logLine("fan command topic=" + fanCommandTopic);
   logLine("pump command topic=" + pumpCommandTopic);
   connectWiFi();
